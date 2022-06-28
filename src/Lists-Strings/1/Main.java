7ce1b124ea89cd754916f6b25e817997b11b52a9
Kacper Płażewski <kacpik@mapik.eu>
2022-06-28 11:24:13 +0200

    Solve Lists, Strings.1

---
+++ src/Lists-Strings/1/Main.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Main {
+    static List<Integer> integerList = List.of(45, 345, 22, 1, 5454, 5311, 3, 0, 1, 3465, 1);
+
+    public static void main(String[] args) {
+        System.out.println(getLargest(integerList));
+    }
+
+    static Integer getLargest(List<Integer> list) {
+        int largest = list.get(0);
+        for (int i : list) {
+            if (i > largest) {
+                largest = i;
+            }
+        }
+        return largest;
+    }
+}
