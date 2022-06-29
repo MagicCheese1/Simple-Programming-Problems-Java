2d24b7941d407692fe6fb8f73ba703c755eb85d4
Kacper Płażewski <kacpik@mapik.eu>
2022-06-29 18:01:06 +0200

    i = i + 1 ?! 😲😩😤

--- src/Elementary/11/Main.java
+++ src/Elementary/11/Main.java
@@ -3,9 +3,9 @@ import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
         double result = 0;
-        for(double i = 1.0; i <= Math.pow(10, 6); i = i + 1.0) {
-            result = result + Math.pow(-1.0,i+1.0)/(2.0*i-1.0);
+        for(double i = 1; i <= Math.pow(10, 6); i++) {
+            result += Math.pow(-1,i+1)/(2*i-1);
         }
-        System.out.println(result * 4.0);
+        System.out.println(result * 4);
     }
 }
